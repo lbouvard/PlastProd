73ce79e1151d7e22b80a401f78d475172d1355e8
Anarion39 <sebastien.frebault@gmail.com>
2015-03-25 13:04:11 +0100

    Mise en place d'une page d'erreur.

--- src/AppBundle/DataFixtures/ORM/LoadStock.php
+++ src/AppBundle/DataFixtures/ORM/LoadStock.php
@@ -15,13 +15,13 @@ class LoadStock extends AbstractFixture implements OrderedFixtureInterface
   {
     // Liste des noms de catégorie à ajouter
     $valeur = array(
-      array(10, null, new \DateTime('2014-11-29 10:50:00'), null, null, 0, 0, 1),
-      array(5, null, new \DateTime('2014-12-27 10:42:54'), null, null, 0, 0, 2),
-      array(25, null, new \DateTime('2014-12-16 07:00:10'), null, null, 0, 0, 3),
-      array(20, null, new \DateTime('2014-12-16 07:00:10'), new \DateTime('2014-12-20 14:12:00'), new \DateTime('2014-12-20 14:12:00'), 1, 0, 3),
-      array(4, null, new \DateTime('2014-12-17 09:12:00'), null, null, 0, 0, 4),
-      array(100, null, new \DateTime('2014-12-20 08:25:25'), null, null, 0, 0, 5),
-      array(25, 'Commentaire', new \DateTime('2015-02-13 07:54:10'), null, null, 0, 0, 6)
+      array( new \DateTime('2014-11-29 10:50:00'), null, 0, 1),
+      array( new \DateTime('2014-12-27 10:42:54'), null,  0, 2),
+      array( new \DateTime('2014-12-16 07:00:10'), null, 0, 3),
+      array( new \DateTime('2014-12-16 07:00:10'), new \DateTime('2014-12-20 14:12:00'),  0, 3),
+      array( new \DateTime('2014-12-17 09:12:00'), null, 0, 4),
+      array( new \DateTime('2014-12-20 08:25:25'), null, 0, 5),
+      array( new \DateTime('2015-02-13 07:54:10'), null, 0, 6)
     );
 
     foreach ($valeur as $ligne) {
@@ -29,14 +29,11 @@ class LoadStock extends AbstractFixture implements OrderedFixtureInterface
       // On crée la société
       $stock = new Stock();
   
-      $stock->setQuantite($ligne[0]);
-      $stock->setCommentaire($ligne[1]);
-      $stock->setDateEntree($ligne[2]);
-      $stock->setDateSortie($ligne[3]);
-      $stock->setDateChg($ligne[4]);
-      $stock->setBitChg($ligne[5]);
-      $stock->setBitSup($ligne[6]);
-      $stock->setProduit($this->getReference('produit_'.$ligne[7]));
+      
+      $stock->setDateEntree($ligne[0]);
+      $stock->setDateSortie($ligne[1]);
+      $stock->setBitSup($ligne[2]);
+      $stock->setProduit($this->getReference('produit_'.$ligne[3]));
  
       // On la persiste
       $manager->persist($stock);
